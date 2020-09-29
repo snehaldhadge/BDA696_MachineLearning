b1c643b75c23935140132f65d4e45b4b10397bc4
snehaldhadge <snehaldhadge@gmail.com>
2020-09-29 13:39:32 -0700

    Adding all the logic of Rolling Average in transform

--- Assignment3_Snehal.py
+++ Assignment3_Snehal.py
@@ -23,7 +23,7 @@ def main():
         .option("driver", "org.mariadb.jdbc.Driver")
         .option(
             "dbtable",
-            "(select Hit,Atbat,batter,bc.game_id,DATE(g.local_date) as l_dt \
+            "(select Hit,Atbat,batter,bc.game_id,(g.local_date) as l_dt \
            FROM batter_counts bc join game g on g.game_id=bc.game_id)batter_counts_all",
         )
         .option("user", "root")
@@ -32,18 +32,23 @@ def main():
     )
     b_counts.createOrReplaceTempView("batter_counts_all")
     b_counts.persist(StorageLevel.DISK_ONLY)
-    results = spark.sql(
-        """SELECT SUM(bc_i.Hit) as Sum_H,SUM(bc_i.atbat) as Sum_A,bc_o.batter,bc_o.game_id \
-                FROM batter_counts_all bc_o JOIN \
-                batter_counts_all bc_i ON bc_i.batter = bc_o.batter AND \
-                bc_i.l_dt BETWEEN DATE_SUB(bc_o.l_dt,100) AND DATE_SUB(bc_o.l_dt,1)  \
-                GROUP BY bc_o.batter, bc_o.game_id \
-                ORDER BY bc_o.game_id """
-    )
+    # results = spark.sql(
+    # """SELECT SUM(bc_i.Hit) as Sum_H,SUM(bc_i.atbat)\
+    #  as Sum_A,bc_o.batter,bc_o.game_id \
+    #             FROM batter_counts_all bc_o JOIN \
+    #             batter_counts_all bc_i ON bc_i.batter = bc_o.batter AND \
+    #             bc_i.l_dt BETWEEN DATE_SUB(bc_o.l_dt,100) AND DATE_SUB(bc_o.l_dt,1)  \
+    #             GROUP BY bc_o.batter, bc_o.game_id \
+    #             ORDER BY bc_o.game_id """
+    # )
+    # batting_avg_cal = BattingAvgCalc(
+    #     inputCols=["Sum_H", "Sum_A"], outputCol="rolling_average"
+    # )
     batting_avg_cal = BattingAvgCalc(
-        inputCols=["Sum_H", "Sum_A"], outputCol="rolling_average"
+        inputCols=["Hit", "atbat"], outputCol="rolling_average"
     )
-    rolling_avg = batting_avg_cal.transform(results)
+    # rolling_avg = batting_avg_cal.transform(results)
+    rolling_avg = batting_avg_cal.transform(b_counts)
     print_heading("Rolling Average Calculation results")
     rolling_avg.show()
     print_heading("100 day Rolling Average for each batter for given game_id")
--- BattingAvgCalc.py
+++ BattingAvgCalc.py
@@ -1,7 +1,9 @@
+import pyspark.sql.functions as func
 from pyspark import keyword_only
 from pyspark.ml import Transformer
 from pyspark.ml.param.shared import HasInputCols, HasOutputCol
 from pyspark.ml.util import DefaultParamsReadable, DefaultParamsWritable
+from pyspark.sql.window import Window
 
 
 class BattingAvgCalc(
@@ -26,7 +28,26 @@ class BattingAvgCalc(
     def _transform(self, dataset):
         input_cols = self.getInputCols()
         output_col = self.getOutputCol()
-        dataset = dataset.withColumn(
-            output_col, dataset[input_cols[0]] / dataset[input_cols[1]]
+
+        # function to calculate number of seconds from number of days
+        def days(i):
+            return i * 86400
+
+        windowSpec = (
+            Window.partitionBy(dataset["batter"])
+            .orderBy(dataset["l_dt"].cast("long"))
+            .rangeBetween(days(-100), days(-1))
         )
+
+        dataset = dataset.withColumn(
+            output_col,
+            func.coalesce(
+                func.sum(dataset[input_cols[0]]).over(windowSpec)
+                / func.sum(dataset[input_cols[1]]).over(windowSpec),
+                func.lit(0.0),
+            ),
+        )
+        # dataset = dataset.withColumn(
+        #     output_col, dataset[input_cols[0]] / dataset[input_cols[1]]
+        # )
         return dataset
