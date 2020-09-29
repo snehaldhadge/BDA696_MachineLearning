672a20ce81994ada97c7637c08cbbb2a66326c4c
snehaldhadge <snehaldhadge@gmail.com>
2020-09-29 10:57:12 -0700

    Adding mariadb connector to be auto downloaded

--- Assignment3_bash.sh
+++ Assignment3_bash.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+wget https://downloads.mariadb.com/Connectors/java/connector-java-2.4.3/mariadb-java-client-2.4.3.jar
+mv  mariadb-java-client-2.4.3.jar .venv/lib/python3.8/site-packages/pyspark/jars/
 # Generating the requirement.dev file
 Echo "Installing all the packages"
 pip-compile --output-file=requirements.dev.txt requirements.dev.in --upgrade
